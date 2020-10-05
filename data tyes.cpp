22a57a0262b9e6e6dfd40435dae7eb1db098735d
selva ganapathy <70571211+selvaganapathy-16@users.noreply.github.com>
2020-10-05 08:51:56 +0530

    Add files via upload

---
+++ data tyes.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	 cout<<"size of int is\t"<< sizeof(int)<<endl;
+	  cout<<"size of char is\t"<< sizeof(char)<<endl;
+	   cout<<"size of float is\t"<< sizeof(float)<<endl;
+	    cout<<"size of double is\t"<<  sizeof(double)<<endl;
+}
---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float n,m,b;
+	cout<<"enter the numbers"<<endl;
+	cin>>n>>m;
+	b=n*m;
+	cout<<"product is "<<b<<endl;
+	
+}
